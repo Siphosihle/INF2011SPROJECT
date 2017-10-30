df92ae08088077337df011d3268e626b401602ef
GXXHAN001 <GXXHAN001@UG45>
2017-10-30 21:53:48 +0200

    1

--- HomeScreen/Presentation Layer/ConfirmGuest.cs
+++ HomeScreen/Presentation Layer/ConfirmGuest.cs
@@ -16,5 +16,10 @@ namespace HomeScreen.Presentation_Layer
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
