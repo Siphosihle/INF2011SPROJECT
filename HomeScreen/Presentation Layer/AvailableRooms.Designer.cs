bcff5d3bde1675e255f36335e056b993746cb347
GXXHAN001 <GXXHAN001@UG45>
2017-10-30 21:48:44 +0200

    1

--- HomeScreen/Presentation Layer/AvailableRooms.Designer.cs
+++ HomeScreen/Presentation Layer/AvailableRooms.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.picBox = new System.Windows.Forms.PictureBox();
-            this.lbAvailableRooms = new System.Windows.Forms.Label();
+            this.lblAvailableRooms = new System.Windows.Forms.Label();
             this.btnNext = new System.Windows.Forms.Button();
             this.btnPrevious = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlAvailability = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.pnlAvailability.SuspendLayout();
             this.SuspendLayout();
             // 
             // picBox
@@ -47,16 +48,16 @@
             this.picBox.TabIndex = 27;
             this.picBox.TabStop = false;
             // 
-            // lbAvailableRooms
+            // lblAvailableRooms
             // 
-            this.lbAvailableRooms.AutoSize = true;
-            this.lbAvailableRooms.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbAvailableRooms.ForeColor = System.Drawing.Color.HotPink;
-            this.lbAvailableRooms.Location = new System.Drawing.Point(396, 81);
-            this.lbAvailableRooms.Name = "lbAvailableRooms";
-            this.lbAvailableRooms.Size = new System.Drawing.Size(275, 31);
-            this.lbAvailableRooms.TabIndex = 28;
-            this.lbAvailableRooms.Text = "AVAILABLE ROOMS";
+            this.lblAvailableRooms.AutoSize = true;
+            this.lblAvailableRooms.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvailableRooms.ForeColor = System.Drawing.Color.HotPink;
+            this.lblAvailableRooms.Location = new System.Drawing.Point(396, 81);
+            this.lblAvailableRooms.Name = "lblAvailableRooms";
+            this.lblAvailableRooms.Size = new System.Drawing.Size(275, 31);
+            this.lblAvailableRooms.TabIndex = 28;
+            this.lblAvailableRooms.Text = "AVAILABLE ROOMS";
             // 
             // btnNext
             // 
@@ -81,13 +82,14 @@
             this.btnPrevious.Text = "Previous";
             this.btnPrevious.UseVisualStyleBackColor = true;
             // 
-            // panel1
+            // pnlAvailability
             // 
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(293, 139);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(447, 241);
-            this.panel1.TabIndex = 32;
+            this.pnlAvailability.Controls.Add(this.richTextBox1);
+            this.pnlAvailability.Controls.Add(this.label2);
+            this.pnlAvailability.Location = new System.Drawing.Point(293, 139);
+            this.pnlAvailability.Name = "pnlAvailability";
+            this.pnlAvailability.Size = new System.Drawing.Size(447, 241);
+            this.pnlAvailability.TabIndex = 32;
             // 
             // label2
             // 
@@ -99,21 +101,29 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "The Following Rooms Are Availabe";
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 18);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(441, 220);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
             // AvailableRooms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(944, 507);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlAvailability);
             this.Controls.Add(this.btnPrevious);
             this.Controls.Add(this.btnNext);
-            this.Controls.Add(this.lbAvailableRooms);
+            this.Controls.Add(this.lblAvailableRooms);
             this.Controls.Add(this.picBox);
             this.Name = "AvailableRooms";
             this.Text = "Rest easy Hotel";
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.pnlAvailability.ResumeLayout(false);
+            this.pnlAvailability.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,10 +132,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picBox;
-        private System.Windows.Forms.Label lbAvailableRooms;
+        private System.Windows.Forms.Label lblAvailableRooms;
         private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Button btnPrevious;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlAvailability;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
